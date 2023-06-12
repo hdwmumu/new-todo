68c38878d09b22f87211c2135fb67f21851e6f45
hdwmumu <wil0110@naver.com>
2023-06-12 15:40:14 +0900

    counter

---
+++ new-todo/src/features/counter/Counter.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './counterSlice';
+// import styles from './Counter.module.css'
+// import { styled } from 'styled-component';
+
+
+
+
+export function Counter() {
+	const count = useSelector((state) => state.counter.value);
+	const dispatch = useDispatch();
+
+	return (
+		<div>
+			<div>
+				<button
+					aria-label='Increment value'
+					onClick={() => dispatch(increment())}
+				>
+					+
+				</button>
+				<span className='counter'>{count}</span>
+
+				<button
+					aria-label='Decrement value'
+					onClick={() => dispatch(decrement())}
+				>
+					-
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default Counter;
